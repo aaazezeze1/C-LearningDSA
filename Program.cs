b7e6263375b5c0e1647edf987181d0c7c3b3fc00
Amazing Cabiles <amazinggracecabiles@gmail.com>
2024-08-11 21:35:49 +0800

    LinearSearch again

--- Program.cs
+++ Program.cs
@@ -4,48 +4,54 @@ namespace C_LearningDSA
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // Sorting and Searching
+            // Searching 
+            // 5:10:01
 
-            // Quick Sort - divide and conquer approach, uses pivots instead of sorting by subarrays or merge sort
-            // explanation from BroCode https://youtu.be/Vtckgz38QHs?si=LwuCXVP8HCXSiovN
-            // Stackoverflow https://stackoverflow.com/questions/70402/why-is-quicksort-better-than-mergesort
-            // Code from https://teamtreehouse.com/library/algorithms-sorting-and-searching/code-for-quicksort
-            var list = new List<int>() { 32, 100, 1, 2, 29, 28, 88, 3, 50, 67, 37, 1, 57, 20 };
-            var sortedList = Quicksort(list);
-            Console.WriteLine(String.Join(", ", sortedList));
-        }
-        public static List<int> Quicksort(List<int> list)
-        {
-            if (list.Count <= 1)
-            {
-                return list;
-            }
+            // runs in linear time
+            // Code from https://teamtreehouse.com/library/algorithms-sorting-and-searching/code-for-linear-search
+            //var numbers = new [] { 1, 2, 3, 4, 5, 6 };
+            var numbers = Enumerable.Range(1, 10000).ToArray();
 
-            var lessThanPivot = new List<int>();
-            var greaterThanPivot = new List<int>();
-            // pivot starts at the first element
-            int pivot = list[0];
-            int length = list.Count;
-            for (int i = 1; i < length; i++)
+            foreach (var number in numbers)
             {
-                int currentValue = list[i];
-                if (currentValue <= pivot)
+                //int? result = LinearSearch(numbers, number);
+                int? result = GenericLinearSearch(numbers, number);
+
+                if (result != null && result == (number - 1))
                 {
-                    lessThanPivot.Add(currentValue);
+                    Console.WriteLine($"Index for '{number}' is '{result}'");
                 }
                 else
                 {
-                    greaterThanPivot.Add(currentValue);
+                    throw new Exception("Oh no!");
                 }
             }
-            var sortedList = new List<int>();
-            sortedList.AddRange(Quicksort(lessThanPivot));
-            sortedList.Add(pivot);
-            sortedList.AddRange(Quicksort(greaterThanPivot));
+        }
+        public static int LinearSearch(int[] data, int key)
+        {
+            for (int index = 0; index < data.Length; index++)
+            {
+                 if (data[index] == key)
+                 {
+                     return index;
+                 }
+            }
 
-            return sortedList;
+            return -1;
+        }
+        public static int GenericLinearSearch<T>(T[] data, T key)
+        {
+            for (int index = 0; index < data.Length; index++)
+            {
+                if (EqualityComparer<T>.Default.Equals(data[index], key))
+                {
+                    return index;
+                }
+            }
+
+            return -1;
         }
 
     }
