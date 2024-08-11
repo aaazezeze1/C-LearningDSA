7b35e1f2ea773f79b0a527bc5f554e807181e842
Amazing Cabiles <amazinggracecabiles@gmail.com>
2024-08-11 21:48:24 +0800

    Recursive Binary

--- Program.cs
+++ Program.cs
@@ -7,17 +7,34 @@ namespace C_LearningDSA
         public static void Main(string[] args)
         {
             // Searching 
-            // 5:10:01
 
-            // runs in linear time
             // Code from https://teamtreehouse.com/library/algorithms-sorting-and-searching/code-for-linear-search
-            //var numbers = new [] { 1, 2, 3, 4, 5, 6 };
-            var numbers = Enumerable.Range(1, 10000).ToArray();
+            // https://teamtreehouse.com/library/algorithms-sorting-and-searching/code-for-binary-search
 
+            var numbers = new [] { 1, 2, 3, 4, 5, 6 };
+            //var numbers = Enumerable.Range(1, 10000).ToArray();
+
+            // Linear Search Version
+            //foreach (var number in numbers)
+            //{
+            //    //int? result = LinearSearch(numbers, number);
+            //    int? result = GenericLinearSearch(numbers, number);
+
+            //    if (result != null && result == (number - 1))
+            //    {
+            //        Console.WriteLine($"Index for '{number}' is '{result}'");
+            //    }
+            //    else
+            //    {
+            //        throw new Exception("Oh no!");
+            //    }
+            //}
+
+            // Recursive Binary Search Version
             foreach (var number in numbers)
             {
-                //int? result = LinearSearch(numbers, number);
-                int? result = GenericLinearSearch(numbers, number);
+                //int? result = IterativeBinarySearch(numbers, number);
+                int? result = RecursiveBinarySearch(numbers, number);
 
                 if (result != null && result == (number - 1))
                 {
@@ -52,6 +69,66 @@ namespace C_LearningDSA
             }
 
             return -1;
+        }
+
+        // Recursive Binary Search Version
+        public static int? IterativeBinarySearch(int[] data, int key)
+        {
+            var first = 0;
+            var last = data.Length - 1;
+
+            while (first <= last)
+            {
+                var mid = (first + last) / 2;
+
+                if (data[mid] < key)
+                {
+                    first = mid + 1;
+                }
+                else if (data[mid] > key)
+                {
+                    last = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return null;
+        }
+        public static int? RecursiveBinarySearch<T>(T[] data, T key)
+            where T : IComparable<T>
+        {
+            return RecursiveBinarySearch(data, key, 0, data.Length - 1);
+        }
+
+        public static int? RecursiveBinarySearch<T>(
+            T[] data, T key, int first, int last)
+            where T : IComparable<T>
+        {
+            if (first > last)
+            {
+                return null;
+            }
+            else
+            {
+                var mid = (first + last) / 2;
+                var comparison = data[mid].CompareTo(key);
+
+                if (comparison < 0)
+                {
+                    return RecursiveBinarySearch(data, key, mid + 1, last);
+                }
+                else if (comparison > 0)
+                {
+                    return RecursiveBinarySearch(data, key, first, mid - 1);
+                }
+                else
+                {
+                    return mid;
+                }
+            }
         }
 
     }
