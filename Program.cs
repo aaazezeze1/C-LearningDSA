5e8cdbe5fc1dcf4839d3018a0894281f49a92d3a
Amazing Cabiles <amazinggracecabiles@gmail.com>
2024-08-11 21:06:37 +0800

    quicksort again

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace C_LearningDSA
         {
             // Sorting and Searching
 
-            // Quick Sort
-            // code from https://teamtreehouse.com/library/algorithms-sorting-and-searching/code-for-quicksort
+            // Quick Sort - divide and conquer approach, uses pivots instead of sorting by subarrays or merge sort
+            // explanation from BroCode https://youtu.be/Vtckgz38QHs?si=LwuCXVP8HCXSiovN
+            // Stackoverflow https://stackoverflow.com/questions/70402/why-is-quicksort-better-than-mergesort
+            // Code from https://teamtreehouse.com/library/algorithms-sorting-and-searching/code-for-quicksort
             var list = new List<int>() { 32, 100, 1, 2, 29, 28, 88, 3, 50, 67, 37, 1, 57, 20 };
             var sortedList = Quicksort(list);
             Console.WriteLine(String.Join(", ", sortedList));
@@ -23,6 +25,7 @@ namespace C_LearningDSA
 
             var lessThanPivot = new List<int>();
             var greaterThanPivot = new List<int>();
+            // pivot starts at the first element
             int pivot = list[0];
             int length = list.Count;
             for (int i = 1; i < length; i++)
